b2ab2bb027c05e197510dbd862b8c9812a6c3d3b
UmidjonShamsutdinov <umid.2825@gmail.com>
2023-12-05 18:39:08 +0500

    PROJECT

---
+++ src/components/design-updates/DesignUpdates.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import "./DesignUpdates.scss"
+import { Container } from '../../utils/Utils'
+
+const DesignUpdates = () => {
+    const [inp, setInp] = useState("")
+    const [loading, setLoading] = useState(false) 
+
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+        if(inp.trim().length > 0 ){
+                    
+            setLoading(true)
+        }
+        setTimeout(() => {
+            setLoading(false)
+        }, 5000);
+    }
+  return (
+    <div className='design-updates'>
+        <Container>
+            <div className='design-updates-div'>
+                <div className="design__updates-content">
+                    <h2>Want Spotify Design updates sent straight to your inbox</h2>
+                    <p>{loading && inp}</p>
+                </div>
+                <div className="design__updates-input">
+                    <form className="input" onSubmit={handleSubmit}>
+                        <input value={inp} onChange={(e)=>setInp(e.target.value)} type="text" placeholder='We saved a spot for your email'/>
+                        <button type='submit'>SEND</button>
+                    </form>
+                    <p>By clicking send you'll receive occasional emails from Spotify Design. You always have the choice to unsubscribe within every email you receive.</p>
+                </div>
+            </div>
+        </Container>
+    </div>
+  )
+}
+
+export default DesignUpdates
